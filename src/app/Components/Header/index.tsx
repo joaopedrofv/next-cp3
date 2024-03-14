277be9cf79014dd29523977d08a1936773b80080
joaopedrofv <joaopvillagra1098@gmail.com>
2024-03-14 09:18:36 -0300

    Estilização dos componentes Header e Footer

--- src/app/Components/Header/index.tsx
+++ src/app/Components/Header/index.tsx
@@ -1,8 +1,11 @@
+import './styles.css';
+
 const Header = () => {
     return (
         <>
         <header>
-            <p>Aqui vai o header</p>
+            <p className="h1">Melhor plataforma de filmes</p>
+            <img src='/logo.jpg' alt="Amazon Prime Logo" />
         </header>
         </>
     );
